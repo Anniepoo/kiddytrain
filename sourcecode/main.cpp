96ad3fbf790682c7abdf542f1336446e191d84be
brigaton <brigaton@proton.me>
2025-01-11 13:13:45 -0800

    added kid and traincar structs

--- sourcecode/main.cpp
+++ sourcecode/main.cpp
@@ -1,23 +1,45 @@
 #include <raylib.h>
+#include "vector"
 int width = 500;
 int hight = 500;
-int state;
+int titlescreenstate;
+
+struct kid
+{
+    int x;
+    int y;
+    int strenght;
+    int animalid;
+    int stationid;
+    int happiness;
+    int animframe;
+    int animdesync;
+};
+
+struct traincar
+{
+    int x;
+    int y;
+    int animframe;
+    int rotation;
+};
+
 int main()
 {
     InitWindow(width,hight,"test window");
     SetTargetFPS(60);
     while(!WindowShouldClose())
     {
-        if(state == 1 && IsKeyDown(KEY_SPACE))
+        if(titlescreenstate == 1 && IsKeyDown(KEY_SPACE))
         {
-            state = 2;
+            titlescreenstate = 2;
         }
         BeginDrawing();
-        if(state == 1)
+        if(titlescreenstate == 1)
         {
             DrawText("press space",250,250,5,GRAY);
         }
-        if(state == 2)
+        if(titlescreenstate == 2)
         {
             DrawRectangle(250,250,10,10,RED);
         }
