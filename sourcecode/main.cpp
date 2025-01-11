6d675430b29e3497b746185b51fe944cfe430457
brigaton <brigaton@proton.me>
2025-01-11 05:45:59 -0800

    added temporary start screen

--- sourcecode/main.cpp
+++ sourcecode/main.cpp
@@ -1,14 +1,26 @@
 #include <raylib.h>
 int width = 500;
 int hight = 500;
+int state;
 int main()
 {
     InitWindow(width,hight,"test window");
     SetTargetFPS(60);
     while(!WindowShouldClose())
     {
+        if(state == 1 && IsKeyDown(KEY_SPACE))
+        {
+            state = 2;
+        }
         BeginDrawing();
-        DrawRectangle(250,250,10,10,RED);
+        if(state == 1)
+        {
+            DrawText("press space",250,250,5,GRAY);
+        }
+        if(state == 2)
+        {
+            DrawRectangle(250,250,10,10,RED);
+        }
         ClearBackground(RAYWHITE);
         EndDrawing();
     }
